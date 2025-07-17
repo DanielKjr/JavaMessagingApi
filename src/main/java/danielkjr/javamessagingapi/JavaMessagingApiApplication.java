01502f3a47d26182400f0836edfa9854d5354eb6
DanielKjr <danielkjrjensen@gmail.com>
2025-07-17 15:04:43 +0200

    Initial

---
+++ src/main/java/danielkjr/javamessagingapi/JavaMessagingApiApplication.java
@@ -0,0 +1,13 @@
+package danielkjr.javamessagingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMessagingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMessagingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/danielkjr/javamessagingapi/JavaMessagingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package danielkjr.javamessagingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMessagingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
