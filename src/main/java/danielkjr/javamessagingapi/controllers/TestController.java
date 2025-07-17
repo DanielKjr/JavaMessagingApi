f43ddccc1d04abf0ea5da3b1eeba19479f76ced0
DanielKjr <danielkjrjensen@gmail.com>
2025-07-17 15:40:01 +0200

    Basic setup with docker conneciton

---
+++ src/main/java/danielkjr/javamessagingapi/controllers/TestController.java
@@ -0,0 +1,15 @@
+package danielkjr.javamessagingapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/sometghin")
+    @ResponseBody
+    public String hello(){
+        return "Hello World";
+    }
+}
