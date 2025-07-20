91c1a9ad52b011caee6d5d2997c6ae878ceed282
DanielKjr <danielkjrjensen@gmail.com>
2025-07-20 13:54:35 +0200

    Added rabbitmq to compose and tested basic connection

--- src/main/java/danielkjr/javamessagingapi/controllers/TestController.java
+++ src/main/java/danielkjr/javamessagingapi/controllers/TestController.java
@@ -1,5 +1,7 @@
 package danielkjr.javamessagingapi.controllers;
 
+import danielkjr.javamessagingapi.MessageBroker.Receive;
+import danielkjr.javamessagingapi.MessageBroker.Send;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/sometghin")
+    @GetMapping("/something")
     @ResponseBody
     public String hello(){
-        return "Hello World";
+        Send.send();
+        return "Ayo";
+    }
+
+
+    @GetMapping("/something1")
+    @ResponseBody
+    public void rec(){
+        Receive.receive();
+
     }
 }
